c2dc267de422f3d7888ee3a9a2c06dcc51e2f88c
wavaresupriya <wavaresupriya16@gmail.com>
2023-07-19 20:30:59 +0530

    Basic Programming
    Basic Programming

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;// Variable Declaration
+myName="Supriya"; //Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "pune"; //Variable Declaration and Initialization
+console.log(city);
+var pin; //Variable Declaration
+console.log(pin);
