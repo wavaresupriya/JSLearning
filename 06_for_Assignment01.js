bb730198bb98207fa242fa22ea0a6eb5cb245673
wavaresupriya <wavaresupriya16@gmail.com>
2023-07-31 17:55:34 +0530

    Assignments  for loop and while loop
    Assignments  for loop and while loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,34 @@
+// WAP to print number from 5 to 15 by incrementing 1
+for (let index = 5; index <= 15; index++) {
+    console.log(index); 
+}
+console.log(`-------------------------------------`);
+// WAP to print numbers from 50 to 40 by decrementing by 1
+for (let index = 50; index >= 40; index--) {
+    console.log(index); 
+}
+console.log(`-------------------------------------`);
+// WAP to find first 15 odd numbers
+for (let index = 1; index <30 ; index=index+2) {
+    console.log(index); 
+}
+console.log(`-------------------------------------`);
+// WAP to find first 10 even numbers
+for (let index = 0; index <=18 ; index=index+2) {
+    console.log(index); 
+}
+console.log(`-------------------------------------`);
+// WAP to print table like ->5 10 15 20 25....50
+for (let index = 5; index <=50 ; index=index+5){
+console.log(index);
+}
+console.log(`-------------------------------------`);
+// WAP to print table of 10 like 10 20 30.......100
+for(let index = 10; index <=100 ; index=index+10){
+    console.log(index);
+}
+console.log(`-------------------------------------`);
+// WAP to print table of 10 in reverse order like -> 100 90 80 70......10
+for(index=100 ;index>=10 ;index=index-10){
+    console.log(index);
+}
---
+++ 06_while_Assignment01.js
@@ -0,0 +1,49 @@
+// while loop programs
+// WAP to print number from 5 to 15 by incrementing 1
+var i = 5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`--------------------------------`);
+// WAP to print numbers from 50 to 40 by decrementing by 1
+var i = 50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+console.log(`--------------------------------`);
+// WAP to find first 15 odd numbers
+var i = 1;
+while (i<30) {
+    console.log(i);
+    i=i+2;
+}
+console.log(`--------------------------------`);
+// WAP to find first 10 even numbers
+var i = 0;
+while (i<=18) {
+    console.log(i);
+    i=i+2;
+}
+console.log(`--------------------------------`);
+// WAP to print table like ->5 10 15 20 25....50
+var i = 5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+}
+console.log(`--------------------------------`);
+// WAP to print table of 10 like 10 20 30.......100
+var i = 10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+}
+console.log(`--------------------------------`);
+// WAP to print table of 10 in reverse order like -> 100 90 80 70......10
+var i = 100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+}
