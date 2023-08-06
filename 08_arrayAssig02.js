e4d297a2ba481bd86b5d97d913a297925eee73b5
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-06 22:11:38 +0530

    Update Array Assignment02
    Update Array Assignment02

--- 08_arrayAssig02.js
+++ 08_arrayAssig02.js
@@ -1,9 +1,9 @@
 const arrayNumbers = [20,31,40,25,23,11,29,9,60,2,11];
-console.log(`------------------------------------------------------------------`);
+console.log(`----------------------------Step1--------------------------------------`);
 
 console.log(`Total number of elements in array is: ${arrayNumbers.length}`);
 
-console.log(`-----------------------------------------------------------------`);
+console.log(`------------------------------Step2-----------------------------------`);
 
 let element1 = arrayNumbers[0];
 console.log(`Element stored at index 1 is: ${element1}`);
@@ -11,12 +11,12 @@ let arrayLength = arrayNumbers.length;
 let elementLast = arrayNumbers[arrayLength-1];
 console.log(`Last element is : ${elementLast}`);
 
-console.log(`------------------------------------------------------------------`);
+console.log(`-------------------------------Step3-----------------------------------`);
 let arrayNumbersLength = arrayNumbers.length;
 let thirdLastElement = arrayNumbers[arrayNumbersLength-3];
 console.log(`Last element is : ${thirdLastElement}`);
 
-console.log(`------------------------------------------------------------------`);
+console.log(`--------------------------------step4----------------------------------`);
 //find all even numbers
 for (let index = 0; index < arrayNumbers.length; index++) {
         const element = arrayNumbers[index];
@@ -25,7 +25,7 @@ for (let index = 0; index < arrayNumbers.length; index++) {
     }
 }
 
-console.log(`------------------------------------------------------------`);
+console.log(`--------------------------Step5----------------------------------`);
 //find all odd numbers
 for (let index = 0; index < arrayNumbers.length; index++) {
         const element = arrayNumbers[index];
@@ -33,7 +33,40 @@ for (let index = 0; index < arrayNumbers.length; index++) {
         console.log(element); 
     }
 }
-console.log(`-----------------------------------------------------------------`);
+console.log(`--------------------------Step6----------------------------------`);
+//sum of all the even positioned elements
+let count=0;
+for (let index = 0; index < arrayNumbers.length; index++) {
+   let element=arrayNumbers[index];
+    if (index%2==0) {
+count=count+element;       
+       
+}
+}
+console.log(`Sum of all the even positioned element: ${count}`); 
+
+console.log(`--------------------------Step7----------------------------------`);
+//sum of all the odd positioned elements 
+let sumOdd=0;
+for (let index = 0; index < arrayNumbers.length; index++) {
+   let element=arrayNumbers[index];
+    if (index%2!=0) {
+sumOdd=sumOdd+element;       
+       
+}
+}
+console.log(`Sum of all the odd positioned element: ${sumOdd}`);
+console.log(`--------------------------Step8----------------------------------`);
+//sum of all the elements 
+let sumAllElement=0;
+for (let index = 0; index < arrayNumbers.length; index++) {
+   let element=arrayNumbers[index];
+    
+    sumAllElement=sumAllElement+element; 
+    
+}
+console.log(`Sum of all the element: ${sumAllElement}`);
+console.log(`---------------------------Step9--------------------------------------`);
 //the number which are multiple of 5
 for(let index = 0; index<arrayNumbers.length; index++){
     const element=arrayNumbers[index];
@@ -42,18 +75,24 @@ for(let index = 0; index<arrayNumbers.length; index++){
     }
 }
 
-console.log(`-----------------------------------------------------------------`);
+console.log(`-----------------------------Step10------------------------------------`);
+// is number 155 is Available in array
 let search=arrayNumbers.includes(155);
 console.log(` is 155 available: ${search}`);
 
-console.log(`-----------------------------------------------------------------`);
+console.log(`------------------------------Step11-----------------------------------`);
+//// is number 23 is Available in array
 let searchNum=arrayNumbers.includes(23);
 console.log(`is 23 available: ${searchNum}`);
 
-console.log(`-----------------------------------------------------------------`);
-arrayNumbers.splice(3,0,55,66);
+console.log(`-------------------------------Step12----------------------------------`);
+//insert numbers 55,66 before index 3
 console.log(arrayNumbers);
-console.log(`-----------------------------------------------------------------`);
+let insert=arrayNumbers.splice(3,0,55,66);
+console.log(insert);
+console.log(arrayNumbers);
+console.log(`------------------------------Step13-----------------------------------`);
+console.log(arrayNumbers);
 let removeElement=arrayNumbers.splice(4);
-console.log(arrayNumbers);
 console.log(removeElement);
+console.log(arrayNumbers);
