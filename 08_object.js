40cb71049143cef3ee02518a545c65defc26d67c
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-06 22:14:30 +0530

    object basic and Advance
    object basic and Advance

--- 08_object.js
+++ 08_object.js
@@ -1,14 +1,74 @@
-let personGK = {
+let personGK= {
     empId: 2233,
     state: "MH",
     designation: "Tech Lead",
-    city: "Pune",
+    city: "pune",
     isMarried: true,
     empId: 2233
 }
 console.log(personGK);
 
 console.log(typeof personGK);
-
-console.log(`Emp Id: ${personGK.empId}`); // Dot Notation
+console.log(`Emp Id: ${personGK.empId}`);  //Dot Notation
 console.log(`Emp city: ${personGK["city"]}`);
+
+let isMarried = personGK.isMarried;
+console.log(isMarried);
+
+personGK.city="Mumbai";
+console.log(personGK);
+
+let array = []; //this is empty array
+
+let bank = {
+    bankName: "SBI Bank",
+    accountNumber: 12345673,
+    ifsc: "SBIN000567",
+    address: {
+        street: "Wakad Main Road",
+        pin: 411057,
+        landmark: "Near Petrol Pump",
+        telPhone: "020-446789",
+        city: "Pune",
+        state: "MH",
+        country : "India",
+        getAddress: function(){
+        console.log(`Bank Address: ${this.street}, ${this.landmark}, ${this.pin}, ${this.telPhone}, ${city}, ${this.state}, ${this.country}`);
+        }
+    },
+    empNames : ["Jenny", "Elon", "Warrren", "Stew"]
+}
+
+let city = bank.address.city;
+bank.address.country = "Maharashtra";
+bank.empNames.pop();
+bank.empNames.unshift("Mark");
+
+bank.address.getAddress();
+
+bank.empNames.push("Anil");
+
+
+
+let student = {
+    rollNumber: 2233,
+    state: "MH",
+    city: "Pune",
+    college: "COEP Pune"
+}
+
+let keysStudent = Object.keys(student);
+console.log(keysStudent);
+let valuesStudent = Object.values(student);
+console.log(valuesStudent);
+
+console.log(`----------Traversing an object ------------ `);
+for (const key in student) {
+    if (Object.hasOwnProperty.call(student, key)) {
+        const element = student[key];
+        console.log(`Key: ${key}, value : ${element}`); 
+    }
+}
+
+
+let isAvailable= "college" in student;
