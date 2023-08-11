049a71c9bfd7610ed18577db83c06f953bdc0530
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-12 00:53:22 +0530

    create a class learning
    create a class learning

---
+++ 09_classPerson.js
@@ -0,0 +1,37 @@
+class Student {
+constructor(fullName,rollNo,college,emailId,mobile){
+this.fullName = fullName;
+this.rollNo = rollNo;
+this.college = college;
+this.emailId = emailId;
+this.mobile = mobile; 
+ }
+ show(){
+    console.log(`Student Details is Full Name: ${this.fullName} , Roll Number: ${this.rollNo} College Name: ${this.college},
+    Email Id: ${this.emailId},Mobile: ${this.mobile}`);
+ }
+}
+   let studentJenny= new Student("Jenny Gates",11,"COEP Pune","abc@gmail.com","9096 78 6789");
+   
+   let studentStew= new Student("Stew page", 22, "JSPM","stew@gmail.com","9867 8989 98");
+   
+   let studentElon= new Student("Elon Musk",33,"Standford","stew@gmail.com","9867 8989 98");
+   
+   let studentSatya= new Student("Satya Nadella",44,"ABC","stya@gmail.com","9867 8989 98");
+   
+   let studentSundar= new Student("Sundar Pichai",55,"xyz","sundar@gmail.com","9867 8989 98");
+   
+   const array = [studentJenny,studentStew,studentElon,studentSatya,studentSundar]
+   for (const student of array) {
+      console.log(`Traversing array`);
+      student.show();
+      
+   }
+   for (const key in object) {
+      if (Object.hasOwnProperty.call(object, key)) {
+         const element = object[key];
+         
+      }
+   }
+
+
