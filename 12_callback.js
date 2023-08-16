ef5dccda8a2bb4e9be7b24e7cc32c71b96ac1a2d
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-16 16:35:48 +0530

    callback changes
    callback changes

--- 12_callback.js
+++ 12_callback.js
@@ -1,14 +1,22 @@
-function jennyHomework(callback){
+function jennyHomework(callbackStew, callbackElon){
     console.log(`Jenny started assignment`);
     console.log(`Jenny took some time`);
     console.log(`Jenny Completed All her home work`);
     console.log(`Jenny calling back to stew`);
-    callback();
+    callbackStew();
+    callbackElon();
 }
 
 let stewCopyHomework = function(){
+    console.log(`====================== Stew ========================`);
     console.log(`Thank you Jenny for giving me everything ready`);
     console.log(`Stew started copying`);
     console.log(`Stew copied all homework`);
 }
-jennyHomework(stewCopyHomework);
+
+let elonCopyHomework = function(){
+    console.log(`====================== Elon =======================`);
+    console.log(`Elon is copying homework`);
+    console.log(`Elon copied all homework...`);
+}
+jennyHomework(stewCopyHomework, elonCopyHomework);
