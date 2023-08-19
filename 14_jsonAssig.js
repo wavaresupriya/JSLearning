679633de454063496062170cab3a509a1672c759
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-19 16:22:36 +0530

    changes
    changes

--- 14_jsonAssig.js
+++ 14_jsonAssig.js
@@ -12,15 +12,15 @@ let jsonEmployeeInfo=`{
     },
     "referred-by": "E0012"
 }`
-//
+// Convert JSON format into Object
 const employeeObj=JSON.parse(jsonEmployeeInfo);
 //log role DIV
 console.log('role:',employeeObj.role[0].toUpperCase());
 //log last name Melon
 const fullName=employeeObj.name.split(" ");
 const lastName=fullName[1];
-console.log('Last Name is:',lastName);
-//log date of joing year
+console.log(`Last Name is: ${lastName}`);
+//log date of joining year
 const doj=employeeObj.doj.split("-");
-const joingYear=doj[2];
-console.log(`Joining year`,joingYear);
+const joiningYear=doj[2];
+console.log(`Joining year: ${joiningYear}`);
