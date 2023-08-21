dec286d332d8d1623699d7bb09b7ff7b0e60ea51
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-21 18:50:53 +0530

    Stric Mode
    Stric Mode

---
+++ 15_stricMode.js
@@ -0,0 +1,16 @@
+'use strict';
+// myName = "GK"; // Not allowed
+// console.log(myName);
+
+//var age = 22;
+// delete age; // Not allowed
+
+let student = {
+    fullName: 'Virat Kohli',
+    age: 33,
+    totalRuns: 33000
+}
+
+// delete student; // Not allowed
+
+// function show(num1, num1){
