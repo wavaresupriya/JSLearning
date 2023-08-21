31d624384ed58933e10ab5e0b3c0658dd5f01044
wavaresupriya <wavaresupriya16@gmail.com>
2023-08-21 18:55:09 +0530

    Addition using Rest Parameter Assignment
     Addition using Rest Parameter Assignment

---
+++ 16_restParameter.js
@@ -0,0 +1,19 @@
+function add(num1, num2, ...num ){
+    //console.log(`num1 ${num1},  num2 ${num2}`);
+    //console.log(num);
+    var sum = 0;
+    for (let index = 0; index < num.length; index++) {
+    sum+=num[index];    
+    }
+    
+    let result = num1+num2+sum;
+    console.log(` Addition of num1${num1} ,num2 ${num2} and rest parameter num ${num} is: ${result} `);
+}
+
+add(5, 6);
+add(9, 5, 6);
+add(78, 90, 51, 68);
+add(89, 90, 45,78, 90, 51, 68);
+
+
+
