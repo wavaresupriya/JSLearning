7259dbb5eb3ff7077411e6a163dd0e677ab7e64c
wavaresupriya <wavaresupriya16@gmail.com>
2023-09-29 20:01:06 +0530

    document object model
    document object model

--- 27_script.js
+++ 27_script.js
@@ -21,3 +21,46 @@ for (let index = 0; index < allElementsJsTopics.length; index++) {
     const element = allElementsJsTopics[index].innerHTML;
     console.log(element);
 }
+
+//Changing the attribute using JS
+ const elementFollowMe = document.querySelector('#linkedIn');
+ elementFollowMe.setAttribute('href',"https://www.linkedin.com/login");
+
+ //Creating the new element
+ const elementLi = document.createElement('li');
+ const elementTextNode = document.createTextNode('ES6 features');
+ elementLi.appendChild(elementTextNode);
+ const elementJsTopicList = document.querySelector('#jsTopicList');
+ elementJsTopicList.appendChild(elementLi);
+ 
+
+ //Creating the new element inside div
+ const elementSpan = document.createElement('span');
+ const elementTNFollowMe = document.createTextNode("Follow me on social media");
+ elementSpan.appendChild(elementTNFollowMe);
+
+ //Remove element
+ const elementControlFlow = document.querySelector('#controlFlow');
+ const elementJsTopic = document.querySelector('#jsTopicList');
+ elementJsTopic.removeChild(elementControlFlow);
+
+ // Applying css
+ const elementListHeading = document.querySelector('#listHeading');
+ elementListHeading.style.color = 'red';
+
+ // fontFamily
+
+ // Event Handling
+ function youClickedMe(){
+    console.log('Hey you click me...');
+    alert('You clicked me...');
+ }
+
+ const elementSelector = document.querySelector('#selector');
+ elementSelector.addEventListener('click',()=>{
+alert('Hey I am selector and you clicked me. let me hit you back');
+ });
+
+ elementSelector.addEventListener('mouseover',()=>{
+      elementSelector.style.color = 'blue';
+ });
